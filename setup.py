b440dc6474915990be2b10bbc0434445ba4c236b
Lukas Barth <lukas.barth@kit.edu>
2018-02-16 14:28:29 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='tableprinter',
-      version='0.1',
-      description='Yet another python table printer. This one outputs your tables to HTML, LaTeX and ASCII. It can deal with hierarchical rows / columns, output in color, ...',
+      version='0.2',
+      description='Yet another python table printer. This one outputs your tables to HTML, LaTeX, Unicode and ASCII. It can deal with hierarchical rows / columns, output in color, ...',
       url='https://github.com/tinloaf/tableprinter',
       author='Lukas Barth',
       author_email='pypi@mbox.tinloaf.de',
